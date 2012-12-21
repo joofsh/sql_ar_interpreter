6ba2b9ed8a18f857c0ebc330a16fd5c60069f60c
joofsh <jonathanpagano@gmail.com>
2012-12-21 11:13:14 -0500

    Improve logic for ar_query result

--- app/models/query.rb
+++ app/models/query.rb
@@ -7,12 +7,21 @@ class Query
   end
 
   def ar_query
-    find_by_sql_default
+    verbose_ar
+  end
+
+  def verbose_ar
+    return invalid_query unless query_class
+    "#{query_class}#{select_clause}#{join_clause}#{inner_join_clause}#{where_clause}#{order_clause}#{limit_clause}"
   end
 
   def find_by_sql_default
-    return "Not a valid SQL query" unless query_class
+    return invalid_query unless query_class
     "#{query_class}.find_by_sql(\"#{sql_query}\")"
+  end
+
+  def invalid_query
+    "Not a valid SQL query"
   end
 
   def sql_tree
@@ -31,18 +40,20 @@ class Query
     parsed_sql["from"].first.singularize.capitalize
   end
 
+
   def select_clause
-    return nil if "*" == parsed_sql["select"]
+    return nil if parsed_sql.count > 2
+    return ".all" if "*" == parsed_sql["select"]
     ".select(\"#{parsed_sql["select"]}\")"
   end
 
-  def optional_clause sql_verb , ar_method = sql_verb
+  def optional_clause sql_verb, ar_method = sql_verb
     return nil unless parsed_sql[sql_verb]
     ".#{ar_method}(\"#{parsed_sql[sql_verb]}\")"
   end
 
   def join_clause
-    return nil unless parsed_sql["from"].count > 1
+    return nil unless (parsed_sql["from"] && parsed_sql["from"].count > 1)
     clause = ""
     parsed_sql["from"][1..-1].each do |q|
       clause << ".joins(:#{q})"
@@ -54,4 +65,16 @@ class Query
     optional_clause "order by", "order"
   end
 
+  def limit_clause
+    optional_clause "limit"
+  end
+
+  def inner_join_clause
+    return nil unless parsed_sql["inner join"]
+    ".joins(:#{parsed_sql["inner join"]})"
+  end
+
+  def where_clause
+    optional_clause "where"
+  end
 end
--- spec/models/query_spec.rb
+++ spec/models/query_spec.rb
@@ -5,22 +5,36 @@ describe Query do
     @good_query = Query.new sql_query: "select * from users, guests where id = 5 order by name limit 2"
     @bad_query = Query.new sql_query: "foo bar pie"
     @where_tests = YAML.load_file 'spec/treetop/where_testcases.yml'
+    @bland_query = Query.new sql_query: "select * from users"
   end
 
   it 'responds to parsing related methods' do
     @good_query.should respond_to :sql_tree
     @good_query.should respond_to :parsed_sql
+    @good_query.should respond_to :find_by_sql_default
     @good_query.should respond_to :query_class
-    @good_query.should respond_to :default_ar
+    @good_query.should respond_to :select_clause
+    @good_query.should respond_to :optional_clause
+    @good_query.should respond_to :join_clause
+    @good_query.should respond_to :order_clause
+    @good_query.should respond_to :limit_clause
+    @good_query.should respond_to :inner_join_clause
+    @good_query.should respond_to :invalid_query
   end
 
   describe 'sql_tree method' do
-    it 'provides parse tree for good queries' do
+    before do
+
+    end
+    it 'returns parse tree for good queries' do
       @good_query.sql_tree.should be_an_instance_of QueryGrammar::ParentQuery
       @good_query.sql_tree.elements.first.class.should eq QueryGrammar::VerbWithQuery
     end
 
-    it 'provides nil for bad queries' do
+    it 'does not get tripped up by extra whitespace or by semicolon' do
+    end
+
+    it 'returns nil for bad queries' do
       @bad_query.sql_tree.should_not be_an_instance_of QueryGrammar::ParentQuery
       @bad_query.sql_tree.should be_an_instance_of NullParse
     end
@@ -28,13 +42,13 @@ describe Query do
   end
 
   describe 'parsed_sql method' do
-      it 'provides a hash for good queries' do
+      it 'returns a hash for good queries' do
         @good_query.parsed_sql.should be_an_instance_of Hash
         @good_query.parsed_sql.first.class.should eq Array
         @good_query.parsed_sql.first.first.class.should eq String
       end
 
-      it 'provides the correct key/value pairs for good queries' do
+      it 'returns the correct key/value pairs for good queries' do
         @good_query.parsed_sql["select"].should eq "*"
         @good_query.parsed_sql["from"].first.should eq "users"
         @good_query.parsed_sql["from"].second.should eq "guests"
@@ -42,26 +56,26 @@ describe Query do
         @good_query.parsed_sql["limit"].should eq "2"
       end
 
-      it 'provides an array as the value for the FROM pair' do
+      it 'returns an array as the value for the FROM pair' do
         @good_query.parsed_sql["from"].class.should eq Array
         @good_query.parsed_sql["from"].count.should eq 2
       end
 
-      it 'provides error for bad queries' do
+      it 'returns error for bad queries' do
         @bad_query.parsed_sql.should eq "Not a valid SQL query"
       end
   end
 
   describe 'query_class method' do
-    it 'provides an accurate string to use as AM class' do
+    it 'returns an accurate string to use as AM class' do
       @good_query.query_class.class.should eq String
     end
 
-    it 'provides nil for bad queries' do
-      @bad_query.query_class.should eq nil
+    it 'returns nil for bad queries' do
+      @bad_query.query_class.should be_nil
     end
 
-    it 'provides singular & capitalized version of AM class' do
+    it 'returns singular & capitalized version of AM class' do
       @good_query.query_class.should eq "User"
       @good_query.query_class.should_not eq "user"
       @good_query.query_class.should_not eq "users"
@@ -70,7 +84,7 @@ describe Query do
   end
 
   describe 'find_by_sql_default method' do
-    it 'provides valid AR query as string using find_by_sql method' do
+    it 'returns valid AR query as string using find_by_sql method' do
       @good_query.find_by_sql_default.should eq "User.find_by_sql(\"select * from users, guests where id = 5 order by name limit 2\")"
       @where_tests.each do |pair|
         test = Query.new(sql_query: pair["sql"])
@@ -78,7 +92,7 @@ describe Query do
       end
     end
 
-    it 'provides error for invalid queries' do
+    it 'returns error for invalid queries' do
       @bad_query.find_by_sql_default.should eq "Not a valid SQL query"
     end
   end
@@ -88,46 +102,100 @@ describe Query do
       @good_query2 = Query.new sql_query: "select name, id from users"
     end
 
-    it 'defaults to blank if no select clause' do
+    it 'defaults to nil if optional clauses exist' do
       @good_query.select_clause.should be_nil
     end
+    it 'defaults to .all if no select clause' do
+      @bland_query.select_clause.should eq ".all"
+    end
 
-    it 'provides the verb/query pair if exist' do
+    it 'returns the verb/query pair if exist' do
       @good_query2.select_clause.should eq ".select(\"name, id\")"
     end
   end
 
   describe 'optional_clause method' do
     it 'defaults to nil if clause does not exist' do
-      @bad_query.optional_clause("limit").should be_nil
+      @bad_query.optional_clause("limit", ).should be_nil
     end
 
-    it 'provides correct verb/qery pair if exists' do
+    it 'returns correct verb/qery pair if exists' do
       @good_query.optional_clause("limit").should eq ".limit(\"2\")"
     end
   end
 
-  describe 'join_clause method' do
+  describe 'join_clause method for implicit joins' do
     before do
       @one_table = Query.new sql_query: "select * from users"
       @two_tables = @good_query
       @three_tables = Query.new sql_query: "select * from users, guests, admins"
     end
 
-    it 'returns nil if only 1 table' do
+    it 'returns nil if 1 table present (no joins)' do
       @one_table.join_clause.should be nil
+      @bland_query.join_clause.should be nil
     end
 
-    it 'provides correct number of join clauses according to number of tables' do
+    it 'returns correct number of join clauses according to number of tables' do
       @two_tables.join_clause.should eq ".joins(:guests)"
       @three_tables.join_clause.should eq ".joins(:guests).joins(:admins)"
     end
   end
 
+  describe 'inner_join_clause for explicit joins' do
+    before do
+      @inner_join_query = Query.new sql_query: "Select * from users INNER JOIN guests where guests.id = users.id"
+    end
 
-  describe 'order_clause method' do
-    it 'provides correct verb/query pair if exists' do
-      @good_query.order_clause.should eq ".order(\"name\")"
+    it 'returns nil if no inner join clause' do
+      @good_query.inner_join_clause.should be_nil
+    end
+
+    it 'returns correct verb/query pair if exists' do
+      @inner_join_query.inner_join_clause.should eq ".joins(:guests)"
     end
   end
+
+
+  describe 'order_clause method' do
+    it 'returns correct verb/query pair if exists' do
+      @good_query.order_clause.should eq ".order(\"name\")"
+    end
+
+    it 'returns nil if no order clause' do
+      @bland_query.order_clause.should be_nil
+    end
+  end
+
+  describe 'limit_clause method' do
+    it 'returns nil if no limit clause' do
+      @bland_query.limit_clause.should be_nil
+    end
+
+    it 'returns correct verb/query pair if exists' do
+      @good_query.limit_clause.should eq ".limit(\"2\")"
+    end
+  end
+
+  describe 'where_clause method' do
+    it 'returns nil if no where clause' do
+      @bland_query.where_clause.should be_nil
+      @bad_query.where_clause.should be_nil
+    end
+
+    it 'returns correct verb/query pair if exists' do
+      @good_query.where_clause.should eq ".where(\"id = 5\")"
+    end
+  end
+
+  describe 'verbose_ar method' do
+    it 'returns concatenation of all clause methods' do
+      @bland_query.verbose_ar.should eq "User.all"
+    end
+
+    it 'returns correct verbose ar query' do
+      @good_query.verbose_ar.should eq "User.joins(:guests).where(\"id = 5\").order(\"name\").limit(\"2\")"
+    end
+  end
+
 end
