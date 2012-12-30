8768eb19bfb43db157c399b29bdf371d3d09f2dc
jd pagano <jonathanpagano@gmail.com>
2012-12-29 23:22:32 -0500

    Correct ga.js script

--- app/assets/javascripts/ga.js
+++ app/assets/javascripts/ga.js
@@ -1,17 +1,14 @@
-var _gaq;
-
-_gaq = _gaq || [];
+var _gaq = _gaq || [];
 
 _gaq.push(["_setAccount", "UA-37256999-1"]);
 
 _gaq.push(["_trackPreview"]);
 
 (function() {
-  var ga, s;
-  ga = document.createElement("script");
+  var ga = document.createElement("script");
   ga.type = "text/javascript";
   ga.async = true;
   ga.src = ("https:" === document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
-  s = document.getElementByTagName("script")[0];
+  var s = document.getElementsByTagName("script")[0];
   return s.parentNode.insertBefore(ga, s);
 })();
